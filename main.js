f19a3a3d769356885a58a4c87cfbf106ec6394f3
JunyiShi <junyi.shi@yale.edu>
2022-12-13 23:38:45 -0500

    main Png position fixed

--- main.js
+++ main.js
@@ -49,6 +49,20 @@ libraryBtn.addEventListener("click", () => {
 });
 
 const skysNames = [
+  "sunset-sunset-dusk-sky",
+  "white-clouds-adobe-after-effect-cloud-visual-sky",
+  "a-dusty-cloud-thick-cloud-cluster-dark-clouds",
+  "blue-sky-dust-sky-clouds",
+  "cloud-baiyun-sky-cloud",
+  "creative-hand-paintes-blue-and-white-blue-and-white-clouds-watercolor-painting-texture-television",
+  "golden-sky-sunset-sunst-gold-dust",
+  "sky-blue-sky-baiyun",
+  "starry-night-sky-transparent",
+  "backdrop-blue-sky",
+  "orange-sunset-sunset-sky-cloud-dusk",
+];
+
+const fieldsNames = [
   "beyond-the-horizon-grey-road-and-trees-at-distance",
   "Sunny-Grass-Field",
   "green-and-brown-mountain",
@@ -69,9 +83,6 @@ const skysNames = [
   "mountain-idea-meadow-and-mountains",
   "green-grass-background-lawn-wheatgrass-artificial-truf-plant-grass-family",
   "natural-environment-hill-natural",
-];
-
-const fieldsNames = [
   "beyond-the-horizon-grey-road-and-trees-at-distance",
   "Sunny-Grass-Field",
   "green-and-brown-mountain",
@@ -79,17 +90,45 @@ const fieldsNames = [
 ];
 
 const framesNames = [
-  "beyond-the-horizon-grey-road-and-trees-at-distance",
-  "Sunny-Grass-Field",
-  "green-and-brown-mountain",
-  "transparent-paddy-field",
+  "ficture-frame",
+  "square-empty-brown-frame",
+  "antique-frame-square-empty-briwn-frame",
+  "antique-frames-black-and-brown-wooden-frame-thumbnail",
+  "frame-gold-frame-gold-rectangular-brown-ornate-frame-golden-frame",
+  "frames-window-graphy-window-watercolor-painting-frame",
+  "transparent-frame-ornate-gold-picture-ornate-frame-elegance",
+  "transparent-frames-photo-frame-template-phtotshop-transparent-background-picture-silver-metal",
+  "frames-painting-oil-paint-antique-wood-frame-rectangle-mirror-canvas",
+  "vintage-wooden-frame-wooden-frames-pgotos-wooden-board-photo-frame",
+  "yellow-metal-pattern-rectangular-frame-photo-frame-png-image",
+  "rustic-wood-banco-de-images-empty-frame",
+  "vintage-and-classic-golden-frame-on-white-background",
+  "wood-picture-frame",
+  "old-frame-border-png-old-photo-frames",
+  "modern-frame-frame",
+  "retangule-picture-frames-antique-brass-frames-colors-tool-antike-rehmen",
+  "picture-frames-mirror-antique-white-gold-gray-frame",
+  "drawing-painting-photography-aqua-frame",
+  "whhite-frame",
+  "golden-wooden-frame-isolates-transparent",
+  "gold-frame",
+  "dark-antique-frame",
+  "free-gold-vintage-frame-png-fancy-art-frame",
+  "black-frame",
+  "presentation-photo-frames-classic-painting-frame-transparent",
+  "dark-antique-frame",
+  "picture-frame",
+  "golden-frame",
+  "",
 ];
 
 // var pngNames = skysNames[0] + fieldsNames[0] + framesNames[0];
 // console.log(pngNames);
 var pngTitles =
   skysNames[Math.floor(Math.random() * skysNames.length)] +
+  "-" +
   fieldsNames[Math.floor(Math.random() * fieldsNames.length)] +
+  "-" +
   framesNames[Math.floor(Math.random() * framesNames.length)] +
   ".png";
 console.log(pngTitles);
@@ -110,45 +149,42 @@ function setCaption(imgFrame) {
   }, 1500);
 }
 
+let aboutBtn = document.querySelector(".about");
+let intro = document.querySelector(".intro");
+let titleAll = document.querySelectorAll(".title");
+let introOff = true;
 
-let aboutBtn = document.querySelector('.about')
-let intro = document.querySelector('.intro')
-let titleAll = document.querySelectorAll('.title')
-let introOff = true
-
-
-let vw = window.innerWidth
+let vw = window.innerWidth;
 if (vw > 600) {
-  aboutBtn.addEventListener('mouseover', () => {
-    showIntro()
-  })
-  aboutBtn.addEventListener('mouseout', () => {
-    hideIntro()
-  })
+  aboutBtn.addEventListener("mouseover", () => {
+    showIntro();
+  });
+  aboutBtn.addEventListener("mouseout", () => {
+    hideIntro();
+  });
 } else {
-  aboutBtn.addEventListener('click', () => {
+  aboutBtn.addEventListener("click", () => {
     if (introOff) {
-      showIntro()
+      showIntro();
     } else {
-      hideIntro()
+      hideIntro();
     }
-  })
-}  
+  });
+}
 
 function showIntro() {
-  aboutBtn.style.textDecoration = "underline"
-  intro.style.display = "block"
+  aboutBtn.style.textDecoration = "underline";
+  intro.style.display = "block";
   for (let title of titleAll) {
-    title.style.display = "none"
+    title.style.display = "none";
   }
-  introOff = false
+  introOff = false;
 }
 function hideIntro() {
-  aboutBtn.style.textDecoration = "none"
-  intro.style.display = "none"
+  aboutBtn.style.textDecoration = "none";
+  intro.style.display = "none";
   for (let title of titleAll) {
-    title.style.display = "grid"
+    title.style.display = "grid";
   }
-  introOff = true
+  introOff = true;
 }
-
